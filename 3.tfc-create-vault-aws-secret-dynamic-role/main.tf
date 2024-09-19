9c81f1746308159b395c48e1e4b96ed36e99093f
carina-cho <chowai4@gmail.com>
2024-09-19 20:09:50 +0800

    update main.tf

--- 3.tfc-create-vault-aws-secret-dynamic-role/main.tf
+++ 3.tfc-create-vault-aws-secret-dynamic-role/main.tf
@@ -30,8 +30,8 @@ resource "vault_aws_secret_backend_role" "iam_admin_dynamic_role" {
   backend = vault_aws_secret_backend.aws.path
   name = var.secret_role_name.master_iamadmin_role_name
   credential_type = var.credential_type.iam_user
-  #policy_arns = var.policy_arns_name.iamadmin
-   policy_document = <<EOT
+  policy_arns = var.policy_arns_name.iamadmin
+  policy_document = <<EOT
 {
   "Version": "2012-10-17",
   "Statement": [
